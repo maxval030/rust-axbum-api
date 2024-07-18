ef35e020526660879dd43a0610b50dd6a27514cf
“NatMaxVal030” <overtakemadaka@gmail.com>
2024-07-19 01:54:12 +0700

    feat: 🎸 add connent db

---
+++ src/config/db.rs
@@ -0,0 +1,13 @@
+use mongodb::{Client, Database};
+use tracing::log::info;
+
+pub async fn db_connect() -> mongodb::error::Result<Database> {
+    let uri = "mongodb://root:password@0.0.0.0:27017";
+
+    let client = Client::with_uri_str(uri).await?;
+
+    let db = client.database("axum-rest-api");
+
+    info!("Connected to MongoDB");
+    Ok(db)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
+pub mod config;
 pub mod employee;
 pub mod item;
-fn main() {
-    println!("Hello, world!");
+
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+use axum::{http::Method, routing::get, Router};
+use tower_http::cors::{Any, CorsLayer};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    // build our application with a single route
+    let app = Router::new()
+        .layer(
+            CorsLayer::new()
+                .allow_origin(Any)
+                .allow_methods([Method::GET, Method::POST]),
+        )
+        .route("/", get(|| async { "Hello, World!111222" }));
+
+    config::db::db_connect().await.unwrap();
+
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 3000);
+    println!("server start:{}", addr);
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
